592b0388a67bd534271789e64789c8360e441d0f
adlibj <86692507+adlibj@users.noreply.github.com>
2024-03-02 23:17:55 +0800

    Add files via upload

--- jboca_worker.js
+++ jboca_worker.js
@@ -12,24 +12,36 @@ async function recordNumber(evt){
 		self.close()
 		}
 	} else {
-		const fileHandle = evt.data
+		fetch("https://www.boca.gov.tw/sp-wain-board-1.html")
+			.then(
+				(response) => {
+					if(response.ok) {
+						return response.text()
+					}
+				}
+			)
+			.then(
+				(txt) => {
+					const tmpFileHandle = await root.getFileHandle("a.txt", { create: true })
+					const tmpAccessHandle = await tmpFileHandle.createSyncAccessHandle()
+					var fileSize = tmpAccessHandle.getSize()
+					const msg = new TextEncoder().encode((new Date().toLocaleString()) + " " + txt.match(/一般件目前號碼.*?>(\d*?)</)[1] + "\n")
+					tmpAccessHandle.write(msg, { at: fileSize })
+					
+					var fileSize = tmpAccessHandle.getSize()
+					const buffer = new DataView(new ArrayBuffer(fileSize))
+					tmpAccessHandle.read(buffer, { at: 0 })
+					
+					tmpAccessHandle.flush()
+					tmpAccessHandle.close()
+					
+					const fileHandle = evt.data
+					const writableHandle = await fileHandle.createWritable()
+					await writableHandle.write(buffer)
+					await writableHandle.close()
+				}
+			)
 		
-		const tmpFileHandle = await root.getFileHandle("a.txt", { create: true })
-		const tmpAccessHandle = await tmpFileHandle.createSyncAccessHandle()
-		var fileSize = tmpAccessHandle.getSize()
-		const msg = new TextEncoder().encode((new Date().toLocaleString()) + "\n")
-		tmpAccessHandle.write(msg, { at: fileSize })
-		
-		var fileSize = tmpAccessHandle.getSize()
-		const buffer = new DataView(new ArrayBuffer(fileSize))
-		tmpAccessHandle.read(buffer, { at: 0 })
-		
-		tmpAccessHandle.flush()
-		tmpAccessHandle.close()
-		
-		const writableHandle = await fileHandle.createWritable()
-		await writableHandle.write(buffer)
-		await writableHandle.close()
 		
 		self.postMessage("Log updated")
 		self.close()
