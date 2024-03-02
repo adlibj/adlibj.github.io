1b115d52a6ea5ec84bf2df384876b424c2e0cf9b
adlibj <86692507+adlibj@users.noreply.github.com>
2024-03-02 17:35:54 +0800

    Add files via upload

--- jboca_worker.js
+++ jboca_worker.js
@@ -2,11 +2,14 @@ self.onmessage = recordNumber
 
 async function recordNumber(evt){
 	const fileHandle = evt.data
-	const fileRWHandle = await fileHandle.createSyncAccessHandle()
-	const fileSize = fileRWHandle.getSize()
+	
+	const root = await navigator.storage.getDirectory()
+	const tmpFileHandle = await root.getFileHandle("a.txt", { create: true })
+	const tmpAccessHandle = await tmpFileHandle.createSyncAccessHandle()
+	const fileSize = tmpAccessHandle.getSize()
 	const msg = new TextEncoder().encode(new Date().toLocaleString())
-	fileRWHandle.write(msg, { at: fileSize })
-	fileRWHandle.flush()
-	fileRWHandle.close()
+	tmpAccessHandle.write(msg, { at: fileSize })
+	tmpAccessHandle.flush()
+	tmpAccessHandle.close()
 	self.postMessage("file updated")
 }
